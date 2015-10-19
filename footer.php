abacebafad2ade58d0039d7d3f7a35345dfa297c
Sean Reilly <surreily1@gmail.com>
2015-10-19 17:22:48 +0100

    Capitalized the 'p' in WordPress.

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 
 		<div class="footer">
 			<div class="container footer-text">
-				<p><?php echo get_theme_mod('footer_text_content_setting', 'Enter something unique here!'); ?> Powered by <a href="https://wordpress.org/">Wordpress</a> and <a href="https://www.getbootstrap.com/">Bootstrap</a>.</p>
+				<p><?php echo get_theme_mod('footer_text_content_setting', 'Enter something unique here!'); ?> Powered by <a href="https://wordpress.org/">WordPress</a> and <a href="https://www.getbootstrap.com/">Bootstrap</a>.</p>
 			</div>
 		</div>
 		
